f9caafb4c2f9fc32dcf5e20530c921d5ab1cb04f
Lyrach2004 <charlyzoungrana2004@gmail.com>
2026-05-10 19:25:22 +0100

    Sécurisation du controller customer avec OAuth2 et jwt

--- src/main/java/com/lyrachtech/ebankingbackend/web/CustomerRestController.java
+++ src/main/java/com/lyrachtech/ebankingbackend/web/CustomerRestController.java
@@ -1,12 +1,12 @@
 package com.lyrachtech.ebankingbackend.web;
 
-import com.lyrachtech.ebankingbackend.dtos.BankAccountDTO;
 import com.lyrachtech.ebankingbackend.dtos.CustomerBankAccountsDTO;
 import com.lyrachtech.ebankingbackend.dtos.CustomerDTO;
 import com.lyrachtech.ebankingbackend.exceptions.CustomerNotFound;
 import com.lyrachtech.ebankingbackend.services.BankAccountService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,34 +20,40 @@ public class CustomerRestController {
     private final BankAccountService bankAccountService;
 
     @GetMapping("/customers")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public List<CustomerDTO> customers(){
         return bankAccountService.listCustomers();
     };
 
     @GetMapping("/customers/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public CustomerDTO getCustomer(@PathVariable(name="id") Long id) throws CustomerNotFound {
         return bankAccountService.getCustomer(id);
     };
 
     @PostMapping("/customers")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
         return bankAccountService.saveCustomer(customerDTO);
 
     }
 
     @PutMapping("/customers/{customerId}")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public CustomerDTO updateCustomer(@PathVariable Long customerId,@RequestBody CustomerDTO customerDTO){
         customerDTO.setId(customerId);
         return bankAccountService.updateCustomer(customerDTO);
     };
 
     @DeleteMapping("/customers/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public void deleteCustomer(@PathVariable Long id){
         bankAccountService.deleteCustomer(id);
 
     }
 
     @GetMapping("/customers/search")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public List<CustomerDTO> searchCustomers(@RequestParam(name="keyword",defaultValue = "") String keyword){
         return bankAccountService.searchCustomers("%"+keyword+"%");
     };
